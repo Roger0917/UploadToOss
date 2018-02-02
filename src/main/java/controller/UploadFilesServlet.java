ea2130ef802ff4cea464ae7ed8125b1156ad7587
Roger0917 <1421128167@qq.com>
2018-02-02 15:42:00 +0800

    second commit

--- src/main/java/controller/UploadFilesServlet.java
+++ src/main/java/controller/UploadFilesServlet.java
@@ -1,8 +1,8 @@
 package controller;
 
 import com.aliyun.oss.OSSClient;
-import util.AliyunOSSClientUtil;
-import util.OSSClientConstants;
+import util.AliyunOssClientUtil;
+import util.OssClientConstants;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -24,14 +24,14 @@ public class UploadFilesServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //初始化OSSClient
-        OSSClient ossClient=AliyunOSSClientUtil.getOSSClient();
+        OSSClient ossClient=AliyunOssClientUtil.getOSSClient();
         for(Part part:req.getParts()){
             if(part!=null&&part.getName().startsWith("file")){
                 InputStream is=part.getInputStream();
                 String fname=part.getSubmittedFileName();
                 long fileSize=part.getSize();
                 System.out.println("文件名"+fname+"文件类型"+part.getContentType()+"文件大小"+fileSize);
-                String md5key=AliyunOSSClientUtil.uploadObject2OSS(req,resp,ossClient,is,fname,fileSize, OSSClientConstants.BUCKET_NAME,OSSClientConstants.FOLDER);
+                String md5key= AliyunOssClientUtil.uploadObject2OSS(req,resp,ossClient,is,fname,fileSize, OssClientConstants.BUCKET_NAME,OssClientConstants.FOLDER);
                 is.close();
                 //logger.info("上传后的文件MD5数字唯一签名:" + md5key);
                 //上传后的文件MD5数字唯一签名:40F4131427068E08451D37F02021473A
